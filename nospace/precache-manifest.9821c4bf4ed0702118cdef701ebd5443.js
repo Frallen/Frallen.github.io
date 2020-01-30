2cecd3043327535df029e1ca64d6fe3183172885
Frallen <31008998+Frallen@users.noreply.github.com>
2020-01-30 19:48:20 +0300

    Add files via upload

---
+++ nospace/precache-manifest.9821c4bf4ed0702118cdef701ebd5443.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f27f8998f5974a9a106353ec2ab5a28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f69d36fd10838e48476",
+    "url": "/static/css/main.ed8c907c.chunk.css"
+  },
+  {
+    "revision": "82e1a320572c9b320ece",
+    "url": "/static/js/2.4fb265d6.chunk.js"
+  },
+  {
+    "revision": "2f69d36fd10838e48476",
+    "url": "/static/js/main.3e292908.chunk.js"
+  },
+  {
+    "revision": "bf3ffa2590aed8001aff",
+    "url": "/static/js/runtime-main.33e941e6.js"
+  },
+  {
+    "revision": "c196b478fe4a8c23933790986e54e278",
+    "url": "/static/media/EmailAnim.c196b478.svg"
+  },
+  {
+    "revision": "6fead1e4d1a96131fad89bebf655c15e",
+    "url": "/static/media/fire.6fead1e4.svg"
+  },
+  {
+    "revision": "6caacd1ac451f5d5c012ebabcb25d071",
+    "url": "/static/media/firebase.6caacd1a.svg"
+  },
+  {
+    "revision": "3e0fed2f87f41be6dce242ab74b19463",
+    "url": "/static/media/preloader.3e0fed2f.svg"
+  },
+  {
+    "revision": "890c39ce79cbea4a0926018144b2be54",
+    "url": "/static/media/react.890c39ce.svg"
+  },
+  {
+    "revision": "4a27628b6297ce2b32e8c43f80ffbbf8",
+    "url": "/static/media/redux.4a27628b.svg"
+  },
+  {
+    "revision": "b5e415dc1cd7ede0b1ccee6c4537b20e",
+    "url": "/static/media/sadface.b5e415dc.svg"
+  }
+]);
---
+++ nospace/precache-manifest.f8e726051a4df062a4ebd1b0ad4b0070.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0dbf03baad4941a1ba29b020c15bb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e8174011e95aa20bb1f",
+    "url": "/static/css/main.ed1cb247.chunk.css"
+  },
+  {
+    "revision": "8f48e26c18d5c91f75fd",
+    "url": "/static/js/2.c22ec430.chunk.js"
+  },
+  {
+    "revision": "0e8174011e95aa20bb1f",
+    "url": "/static/js/main.ca72c319.chunk.js"
+  },
+  {
+    "revision": "bf3ffa2590aed8001aff",
+    "url": "/static/js/runtime-main.33e941e6.js"
+  },
+  {
+    "revision": "c196b478fe4a8c23933790986e54e278",
+    "url": "/static/media/EmailAnim.c196b478.svg"
+  },
+  {
+    "revision": "8aee73a04ac172f9fc0efa5b5070a0c8",
+    "url": "/static/media/fire.8aee73a0.svg"
+  },
+  {
+    "revision": "04df79aafb6fe943f093739ce261d608",
+    "url": "/static/media/firebase.04df79aa.svg"
+  },
+  {
+    "revision": "3e0fed2f87f41be6dce242ab74b19463",
+    "url": "/static/media/preloader.3e0fed2f.svg"
+  },
+  {
+    "revision": "890c39ce79cbea4a0926018144b2be54",
+    "url": "/static/media/react.890c39ce.svg"
+  },
+  {
+    "revision": "3102fae92fadb55f82faa5fdd8bd75c1",
+    "url": "/static/media/redux.3102fae9.svg"
+  },
+  {
+    "revision": "be24a91d94d625fd1fb3128e97d452dc",
+    "url": "/static/media/sadface.be24a91d.svg"
+  }
+]);
